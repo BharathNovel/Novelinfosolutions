42c2227863c3603433011fb8b5943f4c02e0725e
Bharath <bharath@novelinfosolutions.net>
2013-04-05 11:06:14 +0530

    Initial Commit

---
+++ aaaa/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  aaaa
+//
+//  Created by Vinoth Sudoroli on 05/04/13.
+//  Copyright (c) 2013 Novel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ aaaa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  aaaa
+//
+//  Created by Vinoth Sudoroli on 05/04/13.
+//  Copyright (c) 2013 Novel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
